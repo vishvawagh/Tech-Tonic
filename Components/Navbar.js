742721518ac1afc63ca52c56e6ec3860c3f956bc
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2024-06-23 11:52:50 +0530

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -5,13 +5,12 @@ import { BiTerminal } from "react-icons/bi";
 import { HiSun, HiMoon } from "react-icons/hi";
 import { CgUserlane } from "react-icons/cg";
 import { AiOutlineGoogle } from "react-icons/ai";
-import { auth, provider } from "../Firebase/Firebase";
-import { signInWithPopup, signOut } from "firebase/auth";
-import { IoLogOutOutline } from "react-icons/io5";
-import { SiCodefactor } from "react-icons/si";
+
+import { SiCodefactor, SiGoogle } from "react-icons/si";
 import { IoMdArrowDropdown } from "react-icons/io";
 import Alert from "./Alert";
 import { useDispatch } from "react-redux";
+import "../pages/Auth";
 
 function Navbar({ topics }) {
   const [isMounted, setIsMounted] = useState(false);
@@ -36,48 +35,7 @@ function Navbar({ topics }) {
       setTheme(theme === "light" ? "dark" : "light");
     }
   };
-  const handelSignOut = () => {
-    signOut(auth)
-      .then((res) => {
-        setLogin(false);
-        localStorage.removeItem("user");
-        dispatch({ type: "REMOVE_USER" });
-        setViewAlert(true);
-        setAlertMessage("Hope to see you again !!");
-        setTimeout(() => {
-          setViewAlert(false);
-        }, 2000);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const handelSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then((res) => {
-        const userObj = {
-          name: res.user.displayName,
-          photo: res.user.photoURL,
-          token: res.user.accessToken,
-          uid: res.user.uid,
-        };
-
-        localStorage.setItem("user", JSON.stringify(userObj));
-        dispatch({ type: "STORE_USER", payload: userObj });
-
-        setLogin(true);
-        setViewAlert(true);
-        setAlertMessage(`Hello ${res.user.displayName}`);
-        setTimeout(() => {
-          setViewAlert(false);
-        }, 2000);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
+ 
   return (
     <>
       <Alert show={viewAlert} type="success" message={alertMessage} />
@@ -144,23 +102,15 @@ function Navbar({ topics }) {
                   </span>
                 </a>
               </Link>
+              <Link href="/Auth">
+                <a className="flex items-center mx-2 lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50">
+                  <span className="text-xl">
+                    <AiOutlineGoogle className="text-xl" />
+                     
+                  </span>
+                </a>
+              </Link>
 
-              <button className="flex items-center mx-2 lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50">
-                {isLogin ? (
-                  <span
-                    className="md:flex items-center"
-                    onClick={handelSignOut}
-                  >
-                    <span className="hidden md:block text-sm font-medium">Sign Out</span>
-                    <IoLogOutOutline className="text-xl mx-1" />
-                  </span>
-                ) : (
-                  <span className="md:flex items-center" onClick={handelSignIn}>
-                    <span className="hidden md:block text-sm font-medium"> Sign In</span>
-                    <AiOutlineGoogle className="text-xl mx-1" />
-                  </span>
-                )}
-              </button>
             </div>
           </div>
         </div>
